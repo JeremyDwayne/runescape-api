2225a7ac3de4c129d2fc9bf1c9b21fef8d1fe30a
Jeremy Winterberg <winterjd@uwec.edu>
2017-06-19 07:28:06 -0500

    refactored category to have fetch_response function

--- lib/grand_exchange/category.rb
+++ lib/grand_exchange/category.rb
@@ -12,45 +12,38 @@ module Runescape
       # Use to find all items within a category which start with a specific letter
       def self.find_letter(id, alpha = 'a')
         @page = 1
-        @empty = false
-        @attributes = {}
         @items = []
-        while !@empty do
-          response = Faraday.get("#{ITEMS_URL}category=#{id}&alpha=#{alpha}&page=#{@page}") 
-          attributes = JSON.parse(response.body)
-          @empty = attributes["items"].empty?
-          if !@empty
-            attributes["items"].each { |item| @items << item }
-          else
-            puts "Response Empty: #{id} #{alpha}"
-          end
-          @page += 1
-        end
+        @items = fetch_response(@items, alpha, id)
         new(id, @items)
       end
 
 
       # Use to find all items within a category
       def self.find_all(id)
-        @attributes = {}
         @items = []
         @alphabet = ('a'..'z').to_a << ('%')
         @alphabet.each do |alpha|
-          @empty = false
-          @page = 1
-          while !@empty do
-            response = Faraday.get("#{ITEMS_URL}category=#{id}&alpha=#{alpha}&page=#{@page}") 
-            attributes = JSON.parse(response.body)
-            @empty = attributes["items"].empty?
-            if !@empty
-              attributes["items"].each { |item| @items << item }
-            else
-              puts "Finished Querying Category #{id} Letter: #{alpha}"
-            end
-            @page += 1
-          end
+          @items = fetch_response(@items, alpha, id)
         end
         new(id, @items)
+      end
+
+      def self.fetch_response(items, alpha, id)
+        @empty = false
+        @page = 1
+        @items = items
+        while !@empty do
+          response = Faraday.get("#{ITEMS_URL}category=#{id}&alpha=#{alpha}&page=#{@page}") 
+          attributes = JSON.parse(response.body)
+          if !@empty = attributes["items"].empty?
+            attributes["items"].each { |item| @items << item }
+          else
+            puts "Finished Querying Category #{id} Letter: #{alpha}"
+            break
+          end
+          @page += 1
+        end
+        return @items
       end
 
       def total
