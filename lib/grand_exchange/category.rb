64a1cb640e17a54e370c80c27966b11dc4908d62
Jeremy Winterberg <winterjd@uwec.edu>
2017-06-19 06:31:49 -0500

    made category code not look like dogshit

--- lib/grand_exchange/category.rb
+++ lib/grand_exchange/category.rb
@@ -4,41 +4,18 @@ module Runescape
       attr_reader :misc, :a, :b, :c, :d, :e, :f, :g, :h, :i, :j, :k, :l, :m, :n, :o, :p, :q, :r, :s, :t, :u, :v, :w, :x, :y, :z
 
       def initialize(attributes)
-        alpha = attributes["alpha"]
-        @misc = alpha[0]
-        @a = alpha[1]["items"]
-        @b = alpha[2]["items"]
-        @c = alpha[3]["items"]
-        @d = alpha[4]["items"]
-        @e = alpha[5]["items"]
-        @f = alpha[6]["items"]
-        @g = alpha[7]["items"]
-        @h = alpha[8]["items"]
-        @i = alpha[9]["items"]
-        @j = alpha[10]["items"]
-        @k = alpha[11]["items"]
-        @l = alpha[12]["items"]
-        @m = alpha[13]["items"]
-        @n = alpha[14]["items"]
-        @o = alpha[15]["items"]
-        @p = alpha[16]["items"]
-        @q = alpha[17]["items"]
-        @r = alpha[18]["items"]
-        @s = alpha[19]["items"]
-        @t = alpha[20]["items"]
-        @u = alpha[21]["items"]
-        @v = alpha[22]["items"]
-        @w = alpha[23]["items"]
-        @x = alpha[24]["items"]
-        @y = alpha[25]["items"]
-        @z = alpha[26]["items"]
+        attributes["alpha"].each do |key|
+          k = key["letter"] != "#" ? key["letter"] : "misc"
+          v = key["items"]
+          
+          instance_variable_set("@#{k}", v)
+        end
       end
-
 
       def self.find(id)
         response = Faraday.get("#{CATEGORY_URL}category=#{id}")
         attributes = JSON.parse(response.body)
-        puts attributes.inspect
+        # puts attributes.inspect
         new(attributes)
       end
 
